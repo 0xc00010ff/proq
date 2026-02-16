99a18a8df0f987829379a517cd011b02b7265451
Brian Cox <brian@therealbriancox.com>
2026-02-16 02:04:28 -0800

    Add /test-red route with hello world text

---
+++ src/app/test-red/page.tsx
@@ -0,0 +1,8 @@
+export default function TestRedPage() {
+  return (
+    <div style={{ color: "red", padding: "2rem", fontFamily: "monospace" }}>
+      <h1>Hello World</h1>
+      <p>This is the test-red page.</p>
+    </div>
+  );
+}
